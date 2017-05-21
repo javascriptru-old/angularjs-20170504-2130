975e25e7a347fbe87b4cc8580266ce4db5be930a
DmitryZaharov <dimans88@mail.ru>
2017-05-21 22:34:51 +0300

    ДЗ-2: Приложение "Письма"

---
+++ homework/lesson2-directives/dmitry.zaharov/js/mail-item/mail-item.component.js
@@ -0,0 +1,46 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .component('mailItem', mailItem());
+
+    /* @ngInject */
+    function mailItem() {
+        var component = {
+            controller: MailItemController,
+            controllerAs: 'mi',
+            templateUrl: 'js/mail-item/mail-item.tpl.html',
+            bindings: {
+                mailObj: '<',
+                deleteFn: '&'
+            }
+        };
+        return component;
+    }
+
+    MailItemController.$inject = ['$log'];
+
+    /* @ngInject */
+    function MailItemController($log) {
+        var startTime;
+        var mi = this;
+
+        mi.$onInit = onInit;
+        mi.$onDestroy = onDestroy;
+
+        ////////////////
+
+        function onInit() {
+            startTime = +new Date();
+        }
+
+        function onDestroy() {
+            var lifetime = (+new Date() - startTime) / 1000;
+            $log.info(mi.mailObj.subject + ': ' + lifetime + ' s');
+        }
+
+    }
+
+})();
+
