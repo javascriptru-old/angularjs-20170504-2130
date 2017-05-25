ea5cd97712b0f1cff2e768602d5e590d3bb945ec
dedpnd <pr.deadpanda@gmail.com>
2017-05-25 17:23:49 +0300

    homework #3

---
+++ homework/lesson3-services/dmitrii.baluyk/src/app_main.js
@@ -0,0 +1,12 @@
+/*import './/../node_modules/toastr/build/toastr.css';*/
+import './app.css';
+import 'bootstrap';
+import angular from 'angular';
+
+
+/*toastr = require('toastr');
+import toastr from 'toastr';*/
+
+var app = angular.module("mainApp",[]);
+
+export default app;
